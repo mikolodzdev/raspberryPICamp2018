2572f57f4afcbe08791d43faca388fcee2ae3e4a
olhe <heinzel.oliver@web.de>
2018-09-17 15:59:02 +0200

    added distinguished control

--- inpie/src/button.ts
+++ inpie/src/button.ts
@@ -1,10 +1,48 @@
 import * as Tinkerforge from "tinkerforge";
 import {TinkerforgeConnection} from "./tinkerforgeConnection";
 
+class ButtonControl{
+    static speed: number = .1;
+
+    onValueChange: ((value: number)=>void)|undefined = undefined;
+    private timer: number = 0;
+    private _value: number = 0;
+    private _direction: number = 1;
+
+    get value() : number {return this._value;}
+
+    get increasing(): boolean {return this._direction > 0;}
+
+    start(): void {
+        if(this.timer === 0){
+            this.timer = setInterval(()=>{
+                this.changeValue();
+            }, 100);
+        }
+    }
+
+    stop(): void {
+        if(this.timer !== 0){
+            this._direction = -this._direction;
+            clearInterval(this.timer);
+            this.timer = 0;
+        }
+    }
+
+    private changeValue(): void {
+        let newVal = Math.max(0, Math.min(1, this._value + this._direction * ButtonControl.speed));
+        if(this._value !== newVal){
+            this._value = newVal;
+            if(this.onValueChange !== undefined)
+                this.onValueChange(this._value);
+        }
+    }
+}
 
 export class Button{
     private button: any;
-    private timer: any = undefined;
+    private colorControl: ButtonControl = new ButtonControl();
+    private brightnessControl: ButtonControl = new ButtonControl();
     private _onClicks: Array<()=>void> = [];
 
     constructor(tinkerforgeConnection: TinkerforgeConnection){
@@ -13,20 +51,37 @@ export class Button{
         this.button.on(Tinkerforge.BrickletDualButton.CALLBACK_STATE_CHANGED,
             (buttonL, buttonR, ledL, ledR) => {
                 if(buttonL === Tinkerforge.BrickletDualButton.BUTTON_STATE_PRESSED) {
-                    this.button.setLEDState(ledL, 2 );
+                    this._onClicks.forEach(cb=>cb());
+                    this.colorControl.start();
                 }
                 else if(buttonL === Tinkerforge.BrickletDualButton.BUTTON_STATE_RELEASED) {
-                    this.button.setLEDState(ledL, 3 );
-                    this._onClicks.forEach(cb=>cb());
+                    this.colorControl.stop();
+                    this.updateButtonStates();
                 }
 
                 if(buttonR === Tinkerforge.BrickletDualButton.BUTTON_STATE_PRESSED) {
+                    this._onClicks.forEach(cb=>cb());
+                    this.brightnessControl.start();
                 }
                 else if(buttonR === Tinkerforge.BrickletDualButton.BUTTON_STATE_RELEASED) {
-                    this.toggleTimer();
+                    this.brightnessControl.stop();
+                    this.updateButtonStates();
                 }
             }
         );
+
+        this.brightnessControl.onValueChange = (brightness: number) => {
+            this.setColorAndBrightness(this.colorControl.value, brightness);
+            this.toggleRightButton();
+        }
+        this.colorControl.onValueChange = (color: number) => {
+            this.setColorAndBrightness(color, this.brightnessControl.value);
+            this.toggleLeftButton();
+        }
+    }
+
+    private setColorAndBrightness(color: number, brightness: number): void {
+        console.log('color '  + color + ' brightness ' + brightness);
     }
 
     onClick(cb: ()=>void): void {
@@ -34,19 +89,11 @@ export class Button{
     }
 
     onConnect(): void {
-        this.button.setLEDState(
-            3,
-            3);
+        this.updateButtonStates();
     }
 
-    private toggleTimer(): void {
-        if(this.timer === undefined){
-            this.timer = setInterval(()=>{this.toggleLeftButton()}, 500);
-        }else{
-            clearInterval(this.timer);
-            this.timer = undefined;
-        }
-
+    private updateButtonStates(): void {
+        this.button.setLEDState(this.colorControl.increasing ? 2 : 3, this.brightnessControl.increasing ? 2 : 3);
     }
 
     private toggleLeftButton(): void {
@@ -55,7 +102,15 @@ export class Button{
                 this.button.setLEDState(2, ledR);
             else
                 this.button.setLEDState(3, ledR);
-            this._onClicks.forEach(cb=>cb());
+        });
+    }
+
+    private toggleRightButton(): void {
+        this.button.getLEDState((ledL: number, ledR: number)=>{
+            if(ledR === 3)
+                this.button.setLEDState(ledL, 2);
+            else
+                this.button.setLEDState(ledL, 3);
         });
     }
 }
